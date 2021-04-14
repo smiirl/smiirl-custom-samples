70e3ec8ecf020e5f191f325d7fa6536a9285bdaf
Adrien Vieilleribiere <adrien@smiirl.com>
2021-04-14 16:30:58 +0200

    better failure management

--- samples/php/youtube/youtube_json.php
+++ samples/php/youtube/youtube_json.php
@@ -2,8 +2,8 @@
 
 // change this to match your channel and api key
 // you also should put these credentials in a safer location (for instance in some environment variables)
-//$channelId = "UCKHzE6XF__put_your_real_channel_id";
-//$apiKey = "AIzaSyDod3x-Rxx21cbQMvuWoput_your_real_api_key";
+$channelId = "UCKHzE6XF__put_your_real_channel_id";
+$apiKey = "AIzaSyDod3x-Rxx21cbQMvuWoput_your_real_api_key";
 
 $urlToGet = "https://www.googleapis.com/youtube/v3/channels?id=" . $channelId . "&key=" . $apiKey . "&part=statistics";
 $ch = curl_init();
@@ -20,9 +20,15 @@ curl_setopt_array($ch, [
 $response = curl_exec($ch);
 curl_close($ch);
 $jsonS = json_decode($response);
-$subscribers = $jsonS->items[0]->statistics->subscriberCount;
-$data2 = $jsonS->items[0]->statistics->viewCount;
-$result = ['number' => $subscribers, 'subscribers' => $subscribers, 'views' => $data2];
+$subscribers = 0;
+$views = 0;
+if ($jsonS && $jsonS->items && $jsonS->items[0]) {
+    $subscribers = $jsonS->items[0]->statistics->subscriberCount;
+    $views = $jsonS->items[0]->statistics->viewCount;
+} else {
+    // var_dump($response);
+}
+$result = ['number' => $subscribers, 'subscribers' => $subscribers, 'views' => $views];
 
 echo json_encode($result, JSON_FORCE_OBJECT);
 
