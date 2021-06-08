71297f05be285f7cbca4cf77c0fffad8dc4a78ff
Adrien Vieilleribiere <adrien@smiirl.com>
2021-06-08 16:31:07 +0200

    fix getMyNumber

--- samples/php/vacances/vacances_json.php
+++ samples/php/vacances/vacances_json.php
@@ -8,30 +8,35 @@ use Smiirl\Counter;
 require_once "CalendarTool.php";
 
 
-function getMyNumber($params)
-{
-    $cal = new CalendarTool();
-    $todayTime = time();
-    $targetTime = DateTime::createFromFormat('Y-m-d H:i:s',
-        '2021-07-06 16:42:00')->getTimestamp();
-    $difference = $targetTime - $todayTime;
-    if ($difference < 0) {
-        $difference = 0;
-    }
-    $totalDays = ceil($cal->getRemaining($difference, 'day'));
-    $schoolDays = 0;
-    $tmpTime = $todayTime;
-    for ($i = 0; $i <= ($totalDays); $i++) {
-        $tmpDate = new DateTime();
-        $tmpDate->setTimestamp($tmpTime);
-        $isSchoolDay = $cal->isSchoolDay($tmpDate);
-        if ($isSchoolDay) {
-            $schoolDays++;
+    function getMyNumber($params)
+    {
+        $cal = new CalendarTool();
+        $todayTime = time();
+        //$todayTime = DateTime::createFromFormat('Y-m-d H:i:s', '2021-07-05 13:42:00')->getTimestamp();
+        $todayDateTime =  new DateTime();
+        $todayDateTime->setTimestamp($todayTime);
+        $targetTime = DateTime::createFromFormat('Y-m-d H:i:s',
+            '2021-07-06 16:42:00')->getTimestamp();
+        $targetDateTime = new DateTime();
+        $targetDateTime->setTimestamp($targetTime);
+        $difference = $targetTime - $todayTime;
+        if ($difference < 0) {
+            return 0;
         }
-        $tmpTime = $tmpTime + (60 * 60 * 24);
+        $totalDays = ceil($cal->getRemaining($difference, 'day'));
+        $schoolDays = 0;
+        $tmpTime = $todayTime;
+        for ($i = 0; $i <= ($totalDays); $i++) {
+            $tmpDateTime = new DateTime();
+            $tmpDateTime->setTimestamp($tmpTime);
+            $isSchoolDay = $cal->isSchoolDay($tmpDateTime);
+            if ($isSchoolDay && $tmpDateTime < $targetDateTime) {
+                $schoolDays++;
+            }
+            $tmpTime = $tmpTime + (60 * 60 * 24);
+        }
+        return $schoolDays;
     }
-    return $schoolDays;
-}
 
 
 $counter = new Counter();
